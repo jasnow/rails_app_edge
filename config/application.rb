9dcdb0892bf102a30310695a8fab26042822818d
Al Snow <jasnow@hotmail.com>
2022-04-05 15:06:40 -0400

    Upgraded to latest rails version

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,6 @@ module RailsApp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.active_record.legacy_connection_handling = true
   end
 end
