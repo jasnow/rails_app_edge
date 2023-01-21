68f566800d7864dd1cf4f38ea9f1fdd2bef34527
Al Snow <jasnow@hotmail.com>
2023-01-21 07:28:20 -0500

    Removed '6' from repo name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "rails_app6edge_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "rails_app_edge_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
