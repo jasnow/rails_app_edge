97ee9c069f1d6270bd4b2519e89e56af50fa0ecd
Al Snow <jasnow@hotmail.com>
2023-02-19 19:51:42 -0500

    Modified db files/schema.rb and GH-A yaml file

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,5 @@ module RailsApp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.active_record.legacy_connection_handling = true
   end
 end
